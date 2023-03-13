e8fb4fbf60f9dfcbb7a5c3898f4c851b65d49954
tuanbinh8 <tuanbinh1209@gmail.com>
2023-03-13 16:57:16 +0700

    asofhauish i

--- game.js
+++ game.js
@@ -3,23 +3,89 @@ import { readData, writeData, updateData, changeListener } from './database.js'
 
 let canvas = document.getElementById('canvas')
 let scene = new THREE.Scene()
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+scene.background = getColor('lightblue')
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000)
 let renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, })
 let keys = []
 
-let players, playerComponents = [], player, playerComponent
+let players = [], playerComponents = [], player, playerComponent
 let name = localStorage.name || undefined
-let chatLength
-let hasStarted = false
-let timer
-let mouseChangeX
+
+let authForm = document.getElementById('auth')
+let colorInput = document.getElementById('color')
+if (!name) {
+    let nameInput = authForm.name
+    let passLabel = document.getElementById('pass-label')
+    let passInput = authForm.pass
+    let button = authForm.button
+    nameInput.oninput = async () => {
+        button.style.display = nameInput.value.length && passInput.value.length ? 'block' : 'none'
+        if (await readData('players/' + nameInput.value)) {
+            passLabel.innerHTML = 'Password:'
+            button.innerHTML = 'Log in'
+        } else {
+            passLabel.innerHTML = '(New account) Password:'
+            button.innerHTML = 'Sign up'
+        }
+    }
+    passInput.oninput = async () => {
+        button.style.display = nameInput.value.length && passInput.value.length ? 'block' : 'none'
+        if (await readData('players/' + nameInput.value)) {
+            passLabel.innerHTML = 'Password:'
+            button.innerHTML = 'Log in'
+        } else {
+            passLabel.innerHTML = '(New account) Password:'
+            button.innerHTML = 'Sign up'
+        }
+    }
+    button.onclick = async (event) => {
+        event.preventDefault()
+        let name = nameInput.value
+        let password = passInput.value
+        if (await readData('players/' + name)) {
+            writeData('players/' + name + '/online', true)
+        } else {
+            writeData('players/' + name, {
+                name,
+                password,
+                color: '#ff0000',
+                position: {
+                    x: 0,
+                    y: 0,
+                    z: 0,
+                },
+                rotation: {
+                    x: 0,
+                    y: 0,
+                    z: 0,
+                },
+                online: true,
+            })
+        }
+        localStorage.name = name
+        location.reload()
+    }
+} else {
+    (async function () {
+        authForm.remove()
+        let authContainer = document.getElementById('auth-container')
+        authContainer.remove()
+        if (await readData('players/' + name)) {
+            updateData('players/' + name, {
+                online: true,
+            })
+            start()
+        } else {
+            localStorage.removeItem('name')
+            location.reload()
+        }
+    })()
+}
 
 async function start() {
-    let colorInput = document.getElementById('color')
-    let chatInput = document.getElementById('chat-input')
-    let chatList = document.getElementById('chat-list')
-    let chatButton = document.getElementById('chat-button')
-    let movingButtons = Array.from(document.querySelectorAll('#moving-buttons button'))
+    let loaderContainer = document.getElementById('loader-container')
+    loaderContainer.remove()
+
     let logOutButton = document.getElementById('log-out')
     let playersList = document.getElementById('players-list')
 
@@ -28,32 +94,28 @@ async function start() {
     let dirLight = new THREE.DirectionalLight(0xffffff, 0.5);
     dirLight.position.set(200, 500, 300);
     scene.add(dirLight);
-    scene.background = getColor('lightblue')
-    camera.position.set(0, 200, 200);
-    camera.lookAt(0, 100, 0);
-    let planeGeometry = new THREE.PlaneGeometry(5000, 5000, 5000);
-    let planeMaterial = new THREE.MeshBasicMaterial({ color: getColor('rgb(106, 193, 116)') });
+
+    let grass = new THREE.TextureLoader().load("https://upload.wikimedia.org/wikipedia/commons/4/4c/Grass_Texture.png")
+    grass.anisotropy = 200
+    grass.repeat.set(100, 100)
+    grass.wrapT = THREE.RepeatWrapping
+    grass.wrapS = THREE.RepeatWrapping
+    let planeGeometry = new THREE.PlaneGeometry(10000, 10000, 10000);
+    let planeMaterial = new THREE.MeshLambertMaterial({ map: grass });
     let planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
     planeMesh.rotateX(- Math.PI / 2);
     scene.add(planeMesh)
-    setInterval(updateGameArea)
+
+    let cubeGeo = new THREE.BoxGeometry(50, 50, 50)
+    let cubeMaterial = new THREE.MeshLambertMaterial({ color: getColor('yellow') })
+    let cubeMesh = new THREE.Mesh(cubeGeo, cubeMaterial)
+    cubeMesh.position.set(0, 25, 0)
+    scene.add(cubeMesh)
 
     window.onkeydown = (event) => {
         let key = event.key
         if (!keys.includes(key))
             keys.push(key)
-        if ((key == 't' || key == 'T') && chatInput.style.display == 'none') {
-            event.preventDefault()
-            chatInput.style.display = 'block'
-            chatInput.focus()
-            chatButton.innerText = 'Close'
-        }
-        if (key == 'Escape') {
-            chatInput.style.display = 'none'
-            chatInput.value = ''
-            canvas.focus()
-            chatButton.innerText = 'Chat'
-        }
         if (key == 'Tab') {
             event.preventDefault()
             playersList.style.display = 'block'
@@ -77,176 +139,26 @@ async function start() {
     }
 
     window.onmousemove = (event) => {
-        mouseChangeX = event.movementX
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            mouseChangeX = 0
-        });
-    }
-
-    chatInput.onkeydown = async (event) => {
-        let key = event.key
-        if (key == 'Enter') {
-            chatInput.value = chatInput.value.trim()
-            if (chatInput.value == '') return
-            writeData('chat/' + chatLength, {
-                name,
-                text: chatInput.value,
-            })
-            writeData('chat/length', chatLength + 1)
-            chatInput.value = ''
-        }
-    }
-
-    chatInput.onblur = () => {
-        window.dispatchEvent(new KeyboardEvent('keydown', {
-            'key': 'Escape'
-        }));
+        if (!playerComponent) return
+        playerComponent.rotation.y -= event.movementX / 100
+        playerComponent.rotation.y %= 2 * Math.PI
+        let angle = playerComponent.rotation.y
+        camera.position.x = playerComponent.position.x + 200 * Math.sin(angle)
+        camera.position.y = playerComponent.position.y + 200
+        camera.position.z = playerComponent.position.z + 200 * Math.cos(angle)
     }
 
     colorInput.onchange = () => {
         writeData('players/' + name + '/color', colorInput.value)
     }
 
-    chatButton.onclick = () => {
-        if (chatButton.innerText == 'Chat') {
-            window.dispatchEvent(new KeyboardEvent('keydown', {
-                'key': 't'
-            }));
-        } else {
-            window.dispatchEvent(new KeyboardEvent('keydown', {
-                'key': 'Escape'
-            }));
-        }
-    }
-
-    movingButtons.map(button => {
-        button.ontouchstart = () => {
-            window.dispatchEvent(new KeyboardEvent('keydown', {
-                'key': button.dataset.key
-            }));
-            button.style.backgroundColor = 'white'
-        }
-        button.ontouchend = () => {
-            window.dispatchEvent(new KeyboardEvent('keyup', {
-                'key': button.dataset.key
-            }));
-            button.style.backgroundColor = 'transparent'
-        }
-    })
-
     logOutButton.onclick = () => {
         localStorage.removeItem('name')
         location.reload()
     }
 
-    changeListener('.info/connected', (online) => {
-        if (getPlayer(name)) {
-            if (online) {
-                updateData('players/' + name, {
-                    online: true,
-                })
-            } else {
-                updateData('players/' + name, {
-                    online: false,
-                })
-            }
-        }
-    })
-
-    changeListener('chat', async (data) => {
-        chatList.innerHTML = ''
-        Object.values(data).map(chat => {
-            if (typeof chat == 'object') {
-                if (chat.name)
-                    chatList.innerHTML += `<li><b>${chat.name}:</b> ${chat.text}</li>`
-                else
-                    chatList.innerHTML += `<li>${chat.text}</li>`
-            }
-        })
-        chatLength = await readData('chat/length')
-        chatList.style.clipPath = `polygon(0% ${chatList.offsetHeight - 210}px, 100% ${chatList.offsetHeight - 210}px, 100% 100%, 0% 100%)`
-    })
-
     changeListener('players', async (data) => {
         players = data ? Object.values(data) : []
-        if (!hasStarted) {
-            hasStarted = true
-            if (!name) {
-                let password
-                askName()
-                localStorage.name = name
-                function askName() {
-                    do {
-                        name = prompt('Enter your name')
-                        if (name) name = name.trim()
-                        if (getPlayer(name) && getPlayer(name).online) {
-                            alert('Someone else is using this account')
-                            name = undefined
-                        }
-                    } while (name == '' || !name);
-                    askPassword()
-                }
-                function askPassword() {
-                    password = null
-                    if (getPlayer(name)) {
-                        password = prompt('Enter your password')
-                        if (password) password = password.trim()
-                        if (!password) {
-                            askName()
-                            return
-                        }
-                        if (getPlayer(name).password !== password) {
-                            alert('Invalid password')
-                            askPassword()
-                            return
-                        } else {
-                            updateData('players/' + name, {
-                                online: true,
-                            })
-                        }
-                    } else {
-                        password = prompt('New account. Create a password')
-                        if (password) password = password.trim()
-                        if (password == '') {
-                            askPassword()
-                            return
-                        }
-                        if (!password) {
-                            askName()
-                            return
-                        }
-                        writeData('players/' + name, {
-                            name,
-                            password,
-                            color: '#ff0000',
-                            position: {
-                                x: 0,
-                                y: 0,
-                                z: 0,
-                            },
-                            rotation: {
-                                x: 0,
-                                y: 0,
-                                z: 0,
-                            },
-                            online: true,
-                        })
-                    }
-                }
-            } else {
-                if (getPlayer(name)) {
-                    updateData('players/' + name, {
-                        online: true,
-                    })
-                } else {
-                    localStorage.removeItem('name')
-                    location.reload()
-                }
-            }
-            let loaderContainer = document.getElementById('loader-container')
-            loaderContainer.remove()
-        }
         player = getPlayer(name)
         playersList.innerHTML = `<li id='players-number'>Players: ${players.length}</li>`
         players.map(player => {
@@ -262,7 +174,6 @@ async function start() {
                     component = new Player(player.name, player.color, 50, player.position, player.rotation)
                     component.addComponent()
                     if (player.name == name) playerComponent = component
-                    playersList.innerHTML += `<li><span style='background:${component.color};'></span> ${component.name}</li>`
                 }
                 playersList.innerHTML += `<li><span style='background:${component.color};'></span> ${component.name}</li>`
             } else {
@@ -275,19 +186,25 @@ async function start() {
         colorInput.value = player.color
     })
 
-    changeListener('players/' + name + '/online', (online) => {
-        if (hasStarted) {
-            if (online) chatLog(name + ' joined')
-            else chatLog(name + ' disconnected')
+    changeListener('.info/connected', (online) => {
+        if (getPlayer(name)) {
+            if (online) {
+                updateData('players/' + name, {
+                    online: true,
+                })
+            } else {
+                updateData('players/' + name, {
+                    online: false,
+                })
+            }
         }
     })
 
-    if (!/Android|webOS|iPhone|iPad|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        let movingButtonsContainer = document.getElementById('moving-buttons')
-        movingButtonsContainer.remove()
-        chatButton.remove()
-    }
-    canvas.onclick = () => canvas.requestPointerLock()
+    requestAnimationFrame(updateGameArea)
+}
+
+window.onclick = (event) => {
+    if (event.target !== colorInput) canvas.requestPointerLock()
 }
 
 class Player {
@@ -297,9 +214,6 @@ class Player {
         this.width = width
         this.position = position
         this.rotation = rotation
-        this.speedX = 0
-        this.speedY = 0
-        this.speedZ = 0
         let geometry = new THREE.BoxGeometry(width, width, width);
         let material = new THREE.MeshLambertMaterial({ color: getColor(this.color) });
         this.cube = new THREE.Mesh(geometry, material);
@@ -321,33 +235,34 @@ class Player {
             <li>y: ${Math.round(this.rotation.y)}</li>
             <li>z: ${Math.round(this.rotation.z)}</li>`
 
-            playerComponent.rotation.y -= mouseChangeX ? mouseChangeX / 100 : 0
-            playerComponent.rotation.y = playerComponent.rotation.y % (2 * Math.PI)
-            let radius = 200
-            let angle = this.rotation.y
-            camera.position.x = radius * Math.sin(angle)
-            camera.position.z = radius * Math.cos(angle)
-
             updateData('players/' + name, {
                 position: this.position,
                 rotation: this.rotation,
             })
         }
-        this.cube.position.set(this.position.x - player.position.x, this.position.y + this.width / 2, this.position.z - player.position.z);
+        this.cube.position.set(this.position.x, this.position.y + this.width / 2, this.position.z);
         this.cube.rotation.set(this.rotation.x, this.rotation.y, this.rotation.z)
         this.cube.material.color = getColor(this.color)
     }
     moveUp(speed) {
-        this.speedZ = -speed;
+        let angle = this.rotation.y
+        this.position.x -= speed * Math.sin(angle);
+        this.position.z -= speed * Math.cos(angle);
     }
     moveDown(speed) {
-        this.speedZ = speed;
+        let angle = this.rotation.y + Math.PI
+        this.position.x -= speed * Math.sin(angle);
+        this.position.z -= speed * Math.cos(angle);
     }
     moveLeft(speed) {
-        this.speedX = -speed;
+        let angle = this.rotation.y + Math.PI / 2
+        this.position.x -= speed * Math.sin(angle);
+        this.position.z -= speed * Math.cos(angle);
     }
     moveRight(speed) {
-        this.speedX = speed;
+        let angle = this.rotation.y - Math.PI / 2
+        this.position.x -= speed * Math.sin(angle);
+        this.position.z -= speed * Math.cos(angle);
     }
     isComponent() {
         if (playerComponents.filter(component => component.name == this.name).length) return true
@@ -386,35 +301,35 @@ class Player {
 }
 
 function updateGameArea() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    playerComponent.speedX = 0;
-    playerComponent.speedY = 0;
-    playerComponent.speedZ = 0;
-    camera.lookAt(0, 100, 0);
-    if (keys.includes('ArrowLeft')) playerComponent.moveLeft(1)
-    if (keys.includes('ArrowRight')) playerComponent.moveRight(1)
-    if (keys.includes('ArrowUp')) playerComponent.moveUp(1)
-    if (keys.includes('ArrowDown')) playerComponent.moveDown(1)
-    playerComponents.map(component => component.update())
-    renderer.render(scene, camera);
+    try {
+        let windowSize = 1
+        camera.aspect = (window.innerWidth * windowSize) / (window.innerHeight * windowSize);
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth * windowSize, window.innerHeight * windowSize)
+        playerComponent.speedX = 0;
+        playerComponent.speedY = 0;
+        playerComponent.speedZ = 0;
+        if (keys.includes('w') || keys.includes('W')) playerComponent.moveUp(10)
+        if (keys.includes('a') || keys.includes('A')) playerComponent.moveLeft(10)
+        if (keys.includes('d') || keys.includes('D')) playerComponent.moveRight(10)
+        if (keys.includes('s') || keys.includes('S')) playerComponent.moveDown(10)
+        playerComponents.map(component => component.update())
+        let angle = playerComponent.rotation.y
+        camera.position.x = playerComponent.position.x + 200 * Math.sin(angle)
+        camera.position.y = playerComponent.position.y + 200
+        camera.position.z = playerComponent.position.z + 200 * Math.cos(angle)
+        camera.lookAt(player.position.x, player.position.y + 100, player.position.z);
+        renderer.render(scene, camera);
+    } catch (error) {
+        console.log(error);
+    }
+    requestAnimationFrame(updateGameArea)
 }
 
 function getPlayer(name) {
-    return players.filter(player => player.name == name)[0]
-}
-
-function chatLog(text) {
-    if (text == '' || !text) return
-    writeData('chat/' + chatLength, {
-        text,
-    })
-    writeData('chat/length', chatLength + 1)
+    return name ? players.filter(player => player.name == name)[0] : null
 }
 
 function getColor(color) {
     return new THREE.Color(color)
 }
-
-start()
